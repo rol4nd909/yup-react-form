853db90a7b6ba0fea2110991a65e8074f8ce190a
Rol4nd <mail@rolandfranke.nl>
2023-02-03 12:42:24 +0100

    Initial commit loading state

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,8 @@ type UserSubmitForm = {
 }
 
 function App() {
+  const wait = (timeoutMs: number) => new Promise<void>(resolve => window.setTimeout(() => resolve(), timeoutMs))
+
   const validationSchema = Yup.object().shape({
     fullname: Yup.string().required('Fullname is required'),
     username: Yup.string()
@@ -38,101 +40,111 @@ function App() {
     register,
     handleSubmit,
     reset,
-    formState: { errors }
+    formState: { errors, isSubmitting, isSubmitted }
   } = useForm<UserSubmitForm>({
+    mode: 'onChange',
     resolver: yupResolver(validationSchema)
   });
 
-  const onSubmit = (data: UserSubmitForm) => {
+  const onSubmit = async (data: UserSubmitForm) => {
+    await wait(3000);
+    
     console.log(JSON.stringify(data, null, 2));    
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="form">
+    <>
+      {!isSubmitted ?
+        <form onSubmit={handleSubmit(onSubmit)} className="form">
 
-      <div className={`form__group ${errors.fullname ? 'is-invalid' : ''}`}>
-        <label htmlFor="fullname">Full Name</label>
-        <input 
-          type="text" 
-          {...register('fullname')} 
-          className={`form__input ${errors.fullname ? 'is-invalid' : ''}`} 
-          id="fullname" />
+          <div className={`form__group ${errors.fullname ? 'is-invalid' : ''}`}>
+            <label htmlFor="fullname">Full Name</label>
+            <input 
+              type="text" 
+              {...register('fullname')} 
+              className={`form__input ${errors.fullname ? 'is-invalid' : ''}`} 
+              id="fullname" />
 
-        <div className="form__feedback">{errors.fullname?.message}</div>
-      </div>
+            <div className="form__feedback">{errors.fullname?.message}</div>
+          </div>
 
-      <div className={`form__group ${errors.username ? 'is-invalid' : ''}`}>
-        <label htmlFor="username">User Name</label>
-        <input 
-          type="text" 
-          {...register('username')} 
-          className={`form__input ${errors.username ? 'is-invalid' : ''}`} 
-          autoComplete="username"
-          id="username" />
+          <div className={`form__group ${errors.username ? 'is-invalid' : ''}`}>
+            <label htmlFor="username">User Name</label>
+            <input 
+              type="text" 
+              {...register('username')} 
+              className={`form__input ${errors.username ? 'is-invalid' : ''}`} 
+              autoComplete="username"
+              id="username" />
 
-        <div className="form__feedback">{errors.username?.message}</div>
-      </div>
+            <div className="form__feedback">{errors.username?.message}</div>
+          </div>
 
-      <div className={`form__group ${errors.email ? 'is-invalid' : ''}`}>
-        <label htmlFor="email">Email</label>
-        <input 
-          type="email" 
-          {...register('email')} 
-          className={`form__input ${errors.email ? 'is-invalid' : ''}`} 
-          id="email" />
+          <div className={`form__group ${errors.email ? 'is-invalid' : ''}`}>
+            <label htmlFor="email">Email</label>
+            <input 
+              type="email" 
+              {...register('email')} 
+              className={`form__input ${errors.email ? 'is-invalid' : ''}`} 
+              id="email" />
 
-        <div className="form__feedback">{errors.email?.message}</div>
-      </div>
+            <div className="form__feedback">{errors.email?.message}</div>
+          </div>
 
-      <div className={`form__group ${errors.password ? 'is-invalid' : ''}`}>
-        <label htmlFor="password">Password</label>
-        <input 
-          type="password"
-          {...register('password')} 
-          className={`form__input ${errors.password ? 'is-invalid' : ''}`} 
-          autoComplete="new-password"
-          id="password" />
+          <div className={`form__group ${errors.password ? 'is-invalid' : ''}`}>
+            <label htmlFor="password">Password</label>
+            <input 
+              type="password"
+              {...register('password')} 
+              className={`form__input ${errors.password ? 'is-invalid' : ''}`} 
+              autoComplete="new-password"
+              id="password" />
 
-        <div className="form__feedback">{errors.password?.message}</div>
-      </div>
+            <div className="form__feedback">{errors.password?.message}</div>
+          </div>
 
-      <div className={`form__group ${errors.confirmPassword ? 'is-invalid' : ''}`}>
-        <label htmlFor="confirmPassword">Confirm Password</label>
-        <input 
-          type="password"
-          {...register('confirmPassword')} 
-          className={`form__input ${errors.confirmPassword ? 'is-invalid' : ''}`} 
-          autoComplete="new-password"
-          id="confirmPassword" />
+          <div className={`form__group ${errors.confirmPassword ? 'is-invalid' : ''}`}>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input 
+              type="password"
+              {...register('confirmPassword')} 
+              className={`form__input ${errors.confirmPassword ? 'is-invalid' : ''}`} 
+              autoComplete="new-password"
+              id="confirmPassword" />
 
-        <div className="form__feedback">{errors.confirmPassword?.message}</div>
-      </div>
+            <div className="form__feedback">{errors.confirmPassword?.message}</div>
+          </div>
 
-      <div className={`form__group ${errors.acceptTerms ? 'is-invalid' : ''}`}>
-        <input 
-          type="checkbox"
-          {...register('acceptTerms')} 
-          id="acceptTerms" />
-        <label htmlFor="acceptTerms" className='form__label'>
-          I have read and agree to the Terms
-        </label>
+          <div className={`form__group ${errors.acceptTerms ? 'is-invalid' : ''}`}>
+            <input 
+              type="checkbox"
+              {...register('acceptTerms')} 
+              id="acceptTerms" />
+            <label htmlFor="acceptTerms" className='form__label'>
+              I have read and agree to the Terms
+            </label>
 
-        <div className="form__feedback">{errors.acceptTerms?.message}</div>
-      </div>
+            <div className="form__feedback">{errors.acceptTerms?.message}</div>
+          </div>
 
-      <div className="form__group">
-          <button className="btn  btn-primary" type="submit">
-            Register
-          </button>
-          <button
-            type="button"
-            onClick={() => reset()}
-            className="btn  btn-warning"
-          >
-            Reset
-          </button>
-        </div>
-    </form>
+          <div className="form__group">
+            <button className="btn  btn-primary" type="submit">
+              Register
+            </button>
+            <button
+              type="button"
+              onClick={() => reset()}
+              className="btn  btn-warning"
+            >
+              Reset
+            </button>
+          </div>
+          
+          {isSubmitting && <div className="form__loader"><span className="srt">loading</span></div>}
+            
+        </form> : 'verstuurd'
+      }
+    </>
   );
 }
 
