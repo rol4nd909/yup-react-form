e2bc9c22f754eff0104a40ea3d6752d85e3f2e19
Rol4nd <mail@rolandfranke.nl>
2023-02-03 10:03:31 +0100

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,138 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
 import './App.css';
 
+type UserSubmitForm = {
+  fullname: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  acceptTerms: boolean;
+}
+
 function App() {
+  const validationSchema = Yup.object().shape({
+    fullname: Yup.string().required('Fullname is required'),
+    username: Yup.string()
+      .required('Username is required')
+      .min(6, 'Username must be at least 6 characters')
+      .max(20, 'Username must not exceed 20 characters'),
+    email: Yup.string()
+      .required('Email is required')
+      .email('Email is invalid'),
+    password: Yup.string()
+      .required('Password is required')
+      .min(6, 'Password must be at least 6 characters')
+      .max(40, 'Password must not exceed 40 characters'),
+    confirmPassword: Yup.string()
+      .required('Confirm Password is required')
+      .oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
+    acceptTerms: Yup.bool().oneOf([true], 'Accept Terms is required')
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm<UserSubmitForm>({
+    resolver: yupResolver(validationSchema)
+  });
+
+  const onSubmit = (data: UserSubmitForm) => {
+    console.log(JSON.stringify(data, null, 2));    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="form">
+
+      <div className={`form__group ${errors.fullname ? 'is-invalid' : ''}`}>
+        <label htmlFor="fullname">Full Name</label>
+        <input 
+          type="text" 
+          {...register('fullname')} 
+          className={`form__input ${errors.fullname ? 'is-invalid' : ''}`} 
+          id="fullname" />
+
+        <div className="form__feedback">{errors.fullname?.message}</div>
+      </div>
+
+      <div className={`form__group ${errors.username ? 'is-invalid' : ''}`}>
+        <label htmlFor="username">User Name</label>
+        <input 
+          type="text" 
+          {...register('username')} 
+          className={`form__input ${errors.username ? 'is-invalid' : ''}`} 
+          autoComplete="username"
+          id="username" />
+
+        <div className="form__feedback">{errors.username?.message}</div>
+      </div>
+
+      <div className={`form__group ${errors.email ? 'is-invalid' : ''}`}>
+        <label htmlFor="email">Email</label>
+        <input 
+          type="email" 
+          {...register('email')} 
+          className={`form__input ${errors.email ? 'is-invalid' : ''}`} 
+          id="email" />
+
+        <div className="form__feedback">{errors.email?.message}</div>
+      </div>
+
+      <div className={`form__group ${errors.password ? 'is-invalid' : ''}`}>
+        <label htmlFor="password">Password</label>
+        <input 
+          type="password"
+          {...register('password')} 
+          className={`form__input ${errors.password ? 'is-invalid' : ''}`} 
+          autoComplete="new-password"
+          id="password" />
+
+        <div className="form__feedback">{errors.password?.message}</div>
+      </div>
+
+      <div className={`form__group ${errors.confirmPassword ? 'is-invalid' : ''}`}>
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input 
+          type="password"
+          {...register('confirmPassword')} 
+          className={`form__input ${errors.confirmPassword ? 'is-invalid' : ''}`} 
+          autoComplete="new-password"
+          id="confirmPassword" />
+
+        <div className="form__feedback">{errors.confirmPassword?.message}</div>
+      </div>
+
+      <div className={`form__group ${errors.acceptTerms ? 'is-invalid' : ''}`}>
+        <input 
+          type="checkbox"
+          {...register('acceptTerms')} 
+          id="acceptTerms" />
+        <label htmlFor="acceptTerms" className='form__label'>
+          I have read and agree to the Terms
+        </label>
+
+        <div className="form__feedback">{errors.acceptTerms?.message}</div>
+      </div>
+
+      <div className="form__group">
+          <button className="btn  btn-primary" type="submit">
+            Register
+          </button>
+          <button
+            type="button"
+            onClick={() => reset()}
+            className="btn  btn-warning"
+          >
+            Reset
+          </button>
+        </div>
+    </form>
   );
 }
 
